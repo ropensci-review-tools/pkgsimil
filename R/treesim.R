b4f885bb233ea430f8ef899f34f5ae4d8f79f3d3
mpadge <mark.padgham@email.com>
2024-08-14 14:58:28 +0200

    add cpp11 structure and test fn

---
+++ R/treesim.R
@@ -0,0 +1,5 @@
+#' treesim function'
+#' @export
+treesim <- function () {
+    cpp_test (letters[1:10])
+}
---
+++ src/treesim.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include <cpp11.hpp>
+
+cpp11::writable::integers cpp_test(const cpp11::strings flist);
