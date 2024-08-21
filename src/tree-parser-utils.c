509d185d8007887c3178827ac6f46052f9fd58ea
mpadge <mark.padgham@email.com>
2024-08-21 14:43:55 +0200

    add tree-sitter src

--- src/tree-parser-utils.c
+++ src/tree-parser-utils.c
@@ -52,3 +52,44 @@ void print_bracket (bool open, int col) {
         }
     }
 }
+
+void print_cursor(const TSTreeCursor *cursor, const char *source_code, bool col, int *brackets) {
+    TSNode cursor_node = ts_tree_cursor_current_node(cursor);
+    uint32_t n_children = ts_node_child_count(cursor_node);
+    const char *field_name = ts_tree_cursor_current_field_name(cursor);
+    if (field_name != NULL) {
+        if (strcmp(field_name, "body") == 0 ||
+            (strcmp(field_name, "rhs") == 0 && n_children > 1) ||
+            strcmp(field_name, "lhs") == 0 ||
+            strcmp(field_name, "consequence") == 0 ||
+            strcmp(field_name, "alternative") == 0 ||
+            strcmp(field_name, "arguments") == 0) {
+            return;
+        }
+        if (strcmp(field_name, "open") == 0) {
+            print_bracket(true, col);
+            brackets[0]++;
+        } else if (strcmp(field_name, "close") == 0) {
+            print_bracket(false, col);
+            brackets[1]++;
+        } else {
+            // copy source code for that node, noting that the char[] is not
+            // null terminated, so can't use standard printf.
+            uint32_t first_byte = ts_node_start_byte(cursor_node);
+            uint32_t last_byte = ts_node_end_byte(cursor_node);
+            uint32_t kBytes = last_byte - first_byte;
+            char these_bytes[kBytes];
+            memcpy(these_bytes, (source_code + first_byte), kBytes);
+            // printf(" %s ", field_name);
+            // printf("field_name = %s:\t", field_name);
+            if (kBytes > 0) {
+                if (strcmp(field_name, "function") == 0 ||
+                    strcmp(field_name, "name") == 0) {
+                    printf(" %.*s ", (int)sizeof(these_bytes), these_bytes);
+                } else {
+                    printf(" %s ", field_name);
+                }
+            }
+        }
+    }
+}
--- src/tree-parser-utils.h
+++ src/tree-parser-utils.h
@@ -4,7 +4,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-// #include <tree_sitter/api.h>
+#include <tree_sitter/api.h>
 
 void *loadfile(char *file);
 void print_bracket (bool open, int col);
+void print_cursor(const TSTreeCursor *cursor, const char *source_code, bool col, int *brackets);
