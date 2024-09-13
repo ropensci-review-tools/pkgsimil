ab7168fbbfb0744424dd39602da884c5a3ee1081
mpadge <mark.padgham@email.com>
2024-09-13 15:24:08 +0200

    implement pkg_simil_embed_fns fn

--- R/embedding-similarity-desc.R
+++ R/embedding-similarity-desc.R
@@ -12,6 +12,8 @@ pkgsimil_embed_desc <- function (pkg_name = NULL) {
     embeddings_to_dists (embeddings, descs$pkg_name)
 }
 
+get_Rd_metadata <- utils::getFromNamespace (".Rd_get_metadata", "tools")
+
 #' Calculate minimal distances between embeddings from function descriptions.
 #'
 #' The embeddings are currently retrieved from the Jina AI API. A distance is
@@ -23,12 +25,67 @@ pkgsimil_embed_desc <- function (pkg_name = NULL) {
 #' @export
 pkgsimil_embed_fns <- function (pkg_name = NULL) {
 
+    descs <- lapply (pkg_name, function (p) get_fn_descs (p))
+    names (descs) <- pkg_name
+
+    embeddings <- lapply (descs, function (d) {
+        input <- lapply (d$desc, function (i) list (text = i))
+        get_embeddings (input)
+    })
+
+    index <- seq_along (embeddings) [-length (embeddings)]
+    dists <- lapply (index, function (i) {
+        emb_i <- embeddings [[i]]
+        res_i <- lapply (seq (i + 1, length (embeddings)), function (j) {
+            emb_j <- embeddings [[j]]
+            emb_ij <- cbind (emb_i, emb_j)
+            rd_nms <- c (
+                paste0 (pkg_name [i], "::", descs [[i]]$rd_name),
+                paste0 (pkg_name [j], "::", descs [[j]]$rd_name)
+            )
+            d_ij <- embeddings_to_dists (cbind (emb_i, emb_j), rd_nms)
+            d_ij$pkg_from <- gsub ("::.*$", "", d_ij$from)
+            d_ij$pkg_to <- gsub ("::.*$", "", d_ij$to)
+            d_ij <- d_ij [which (d_ij$pkg_from != d_ij$pkg_to), ]
+            d_ij$from <- gsub ("^.*::", "", d_ij$from)
+            d_ij$to <- gsub ("^.*::", "", d_ij$to)
+
+            d_ij_from <- dplyr::group_by (d_ij, from) |>
+                dplyr::summarise (d = min (d))
+            d_ij_to <- dplyr::group_by (d_ij, to) |>
+                dplyr::summarise (d = min (d))
+            c (i, j, mean (c (d_ij_from$d, d_ij_to$d)))
+        })
+        do.call (rbind, res_i)
+    })
+    dists <- data.frame (do.call (rbind, dists))
+    names (dists) <- c ("from", "to", "dist")
+    dists$from <- pkg_name [dists$from]
+    dists$to <- pkg_name [dists$to]
+
+    return (dists)
+}
+
+get_fn_descs <- function (pkg_name) {
+
+    rd <- tools::Rd_db (package = pkg_name)
+    descs <- vapply (rd, function (i) {
+        d <- get_Rd_metadata (i, "description")
+    }, character (1L))
+    descs <- gsub ("\\n", " ", descs)
+    descs <- gsub ("\\s+", " ", descs)
+
+    index <- which (!is.na (descs))
+    data.frame (
+        desc = unname (descs),
+        rd_name = names (descs)
+    ) [index, ]
 }
 
 get_pkg_descs <- function (pkg_name) {
     d <- vapply (pkg_name, function (i) {
         res <- utils::packageDescription (pkg = i, fields = "Description")
-        res <- gsub ("\\n", "", res)
+        res <- gsub ("\\n", " ", res)
         gsub ("\\s+", " ", res)
     }, character (1L), USE.NAMES = FALSE)
     index <- which (!is.na (d))
