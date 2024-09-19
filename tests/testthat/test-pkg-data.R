cb5d7565d703bc1db71d725e28a304660e97b7e0
mpadge <mark.padgham@email.com>
2024-09-19 16:41:07 +0200

    test 'get_pkg_code' for exported_only for #2

--- tests/testthat/test-pkg-data.R
+++ tests/testthat/test-pkg-data.R
@@ -4,11 +4,12 @@ test_that ("get pkg local text", {
 
     roxygen2::roxygenise (path) # Generate man files
 
-    txt <- get_pkg_text_local (path)
+    txt <- get_pkg_text (path)
     expect_type (txt, "character")
     expect_length (txt, 1L)
     expect_true (grepl ("##\\s*Functions", txt))
     expect_true (grepl ("#\\s*demo", txt))
+    expect_true (nchar (txt) < 1000) # small test package
 
     code <- get_pkg_code (path)
     expect_type (code, "character")
@@ -16,6 +17,7 @@ test_that ("get pkg local text", {
     expect_false (grepl ("#\\s*demo", code))
     expect_false (grepl ("##\\s*Functions", code))
     expect_true (grepl ("This function does nothing", code, fixed = TRUE))
+    expect_true (nchar (code) < 1000)
 
     # ---- test utils fns -----
     # Identified as code because of markdown
@@ -23,4 +25,23 @@ test_that ("get pkg local text", {
     txt <- gsub ("\\n|#+", "", txt)
     expect_false (text_is_code (txt))
     expect_true (text_is_code (code))
+})
+
+test_that ("get pkg installed text", {
+    pkg <- "cli"
+
+    txt <- get_pkg_text (pkg)
+    expect_type (txt, "character")
+    expect_length (txt, 1L)
+    expect_true (nchar (txt) > 1000)
+    expect_false (text_is_code (txt))
+
+    code <- get_pkg_code (pkg)
+    expect_type (code, "character")
+    expect_length (code, 1L)
+    expect_true (nchar (txt) > 1000)
+    expect_true (text_is_code (code))
+
+    code_exp_only <- get_pkg_code (pkg, exported_only = TRUE)
+    expect_true (nchar (code_exp_only) / nchar (code) < 0.75)
 })
