dead14036dd9cea3c2aa3fb8ecb1a1c42787bbc1
mpadge <mark.padgham@email.com>
2024-09-19 16:24:38 +0200

    add test-load-embeddings for #2

---
+++ tests/testthat/test-load-embeddings.R
@@ -0,0 +1,14 @@
+test_that ("load embeddings", {
+
+    fp <- file.path (tempdir (), "pkgsimil")
+    if (!dir.exists (fp)) {
+        dir.create (fp, recursive = TRUE)
+    }
+    Sys.setenv ("PKGSIMIL_CACHE_DIR" = fp)
+    expect_equal (pkgsimil_cache_path (), fp)
+    saveRDS (embeddings, file.path (fp, "embeddings.Rds"))
+    saveRDS (embeddings_fns, file.path (fp, "embeddings-fns.Rds"))
+
+    expect_identical (embeddings, pkgsimil_load_embeddings ())
+    expect_identical (embeddings_fns, pkgsimil_load_embeddings (fns = TRUE))
+})
