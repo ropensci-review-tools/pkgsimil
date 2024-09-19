95af700ccc38393e53fe7dbf542988ff57fc62e1
mpadge <mark.padgham@email.com>
2024-09-19 13:18:52 +0200

    test embeddings_raw fn for #2

--- tests/testthat/test-get-embeddings.R
+++ tests/testthat/test-get-embeddings.R
@@ -32,3 +32,21 @@ test_that ("embedding_dists fn", {
     expect_type (d$d_code, "double")
     expect_true (all (packages %in% c (d$from, d$to)))
 })
+
+test_that ("raw embeddings", {
+
+    withr::local_envvar (list ("PKGSIMIL_TESTS" = "true"))
+
+    packages <- "cli"
+    emb <- with_mock_dir ("emb_raw", {
+        pkgsimil_embeddings_raw (packages)
+    })
+    expect_type (emb, "list")
+    expect_identical (names (emb), c ("text", "code"))
+    is_mat <- vapply (emb, function (i) length (dim (i)) == 2L, logical (1L))
+    expect_true (all (is_mat))
+    ncol <- vapply (emb, ncol, integer (1L))
+    expect_true (all (ncol == 1L))
+    nrow <- vapply (emb, nrow, integer (1L))
+    expect_true (all (nrow == expected_embedding_length))
+})
