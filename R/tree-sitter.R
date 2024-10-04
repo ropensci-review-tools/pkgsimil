0be616a5b9c117499d87996ae6dbbed9646450ba
mpadge <mark.padgham@email.com>
2024-10-04 12:38:11 +0200

    update treesitter fn docs [ci skip]

--- R/tree-sitter.R
+++ R/tree-sitter.R
@@ -145,7 +145,8 @@ tressitter_calls_in_package <- function (path) {
 }
 
 #' Use "treesitter" to tag all function calls made within local package, and to
-#' associate those calls with package namespaces.
+#' associate those calls with package namespaces. This is used as input to the
+#' \link{pkgsimil_bm25_fn_calls} function.
 #'
 #' @param path Path to local package, or `.tar.gz` file of package source.
 #' @return A `data.frame` of all function calls made within the package, with
