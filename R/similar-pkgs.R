bd92e9ec012b5b9a4208ee55b88215804ac99975
mpadge <mark.padgham@email.com>
2024-10-08 15:41:46 +0200

    return single df from similar-pkgs fn

--- R/similar-pkgs.R
+++ R/similar-pkgs.R
@@ -55,26 +55,39 @@ pkgsimil_similar_pkgs <- function (input,
     if (is.null (embeddings)) {
         embeddings <- pkgsimil_load_data (what = "embeddings", corpus = corpus)
     }
-
     nms_expected <- c ("text_with_fns", "text_wo_fns", "code")
     stopifnot (is.list (embeddings))
     stopifnot (identical (names (embeddings), nms_expected))
 
+    if (is.null (idfs)) {
+        idfs <- pkgsimil_load_data (what = "idfs", corpus = corpus)
+    }
+    stopifnot (is.list (idfs))
+    stopifnot (identical (names (idfs), c ("idfs", "token_lists")))
+
     if (input_is_dir (input)) {
 
         res <- similar_pkgs_from_pkg (input, embeddings)
+        # Then add BM25 from package text:
+        txt_with_fns <- get_pkg_text (input)
+        txt_wo_fns <- rm_fns_from_pkg_txt (txt_with_fns) [[1]]
+        bm25_with_fns <-
+            pkgsimil_bm25 (txt_with_fns, idfs = idfs, corpus = corpus)
+        bm25_wo_fns <- pkgsimil_bm25 (txt_wo_fns, idfs = idfs, corpus = corpus)
+        # bm25 fn returns measures against idfs with and without fns:
+        bm25_with_fns$bm25_wo_fns <- NULL
+        bm25_wo_fns$bm25_with_fns <- NULL
+        bm25_text <- dplyr::left_join (bm25_with_fns, bm25_wo_fns, by = "package")
+        res <- dplyr::left_join (res, bm25_text, by = "package") |>
+            dplyr::relocate (code, .after = dplyr::last_col ())
+
         # Then combine BM25 from function calls with "code" similarities:
-        bm25 <- pkgsimil_bm25_fn_calls (input, corpus = corpus)
-        code_sim <- dplyr::left_join (res$code, bm25, by = "package")
-        res$code <- pkgsimil_rerank (code_sim)
+        bm25_code <- pkgsimil_bm25_fn_calls (input, corpus = corpus) |>
+            dplyr::rename (bm25_code = "bm25")
+
+        res <- dplyr::left_join (res, bm25_code, by = "package")
 
     } else {
-
-        if (is.null (idfs)) {
-            idfs <- pkgsimil_load_data (what = "idfs", corpus = corpus)
-        }
-        stopifnot (is.list (idfs))
-        stopifnot (identical (names (idfs), c ("idfs", "token_lists")))
 
         res <- similar_pkgs_from_text (
             input = input,
@@ -99,12 +112,21 @@ similar_pkgs_from_pkg <- function (input, embeddings) {
 
     options (op)
 
-    npkgs <- ncol (embeddings$text_with_fns)
-    nrow <- nrow (emb$text_with_fns)
-    emb_text <- matrix (emb$text_with_fns, nrow = nrow, ncol = npkgs)
-    d_text <- colSums (sqrt ((emb_text - embeddings$text_with_fns)^2))
-    d_text <- data.frame (package = names (d_text), text = unname (d_text))
+    d_text <- lapply (
+        c ("text_with_fns", "text_wo_fns"),
+        function (what) {
+            npkgs <- ncol (embeddings [[what]])
+            nrow <- nrow (emb [[what]])
+            emb_text <- matrix (emb [[what]], nrow = nrow, ncol = npkgs)
+            d_text <- colSums (sqrt ((emb_text - embeddings [[what]])^2))
+            d_text <- data.frame (package = names (d_text), text = unname (d_text))
+            names (d_text) [2] <- what
+            return (d_text)
+        }
+    )
+    d_text <- dplyr::left_join (d_text [[1]], d_text [[2]], by = "package")
 
+    nrow <- nrow (embeddings$code)
     npkgs <- ncol (embeddings$code)
     emb_code <- matrix (emb$code, nrow = nrow, ncol = npkgs)
     d_code <- colSums (sqrt ((emb_code - embeddings$code)^2))
@@ -112,12 +134,10 @@ similar_pkgs_from_pkg <- function (input, embeddings) {
 
     out <- dplyr::left_join (d_text, d_code, by = "package")
     out$code <- out$code / max (out$code, na.rm = TRUE)
-    out$text <- out$text / max (out$text, na.rm = TRUE)
+    out$text_with_fns <- out$text_with_fns / max (out$text_with_fns, na.rm = TRUE)
+    out$text_wo_fns <- out$text_wo_fns / max (out$text_wo_fns, na.rm = TRUE)
 
-    list (
-        text = order_output (out, "text"),
-        code = order_output (out, "code")
-    )
+    return (out)
 }
 
 order_output <- function (out, what = "text") {
