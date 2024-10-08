106dc2dc75d1732f0b0697d9689fe8dfb7751fc9
mpadge <mark.padgham@email.com>
2024-10-08 16:07:30 +0200

    rewrite print method with new df output

--- R/pkgsimil-methods.R
+++ R/pkgsimil-methods.R
@@ -10,7 +10,11 @@ print.pkgsimil <- function (x, ...) {
     n <- attr (x, "n")
 
     if (inherits (x, "data.frame")) {
-        print (utils::head (x$package, n))
+        xout <- list (
+            "text" = x$package [x$text_rank] [seq_len (n)],
+            "code" = x$package [x$code_rank] [seq_len (n)]
+        )
+        print (xout)
     } else if (inherits (x, "list")) {
         xout <- lapply (x, function (i) utils::head (i$package, n))
         print (xout)
