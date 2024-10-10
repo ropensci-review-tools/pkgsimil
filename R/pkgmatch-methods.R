a5e4408bac5e1f502483270c0559d827e446aa73
mpadge <mark.padgham@email.com>
2024-10-10 11:52:18 +0200

    simplify print method; everything is now a data.frame

--- R/pkgmatch-methods.R
+++ R/pkgmatch-methods.R
@@ -9,22 +9,15 @@ print.pkgmatch <- function (x, ...) {
 
     n <- attr (x, "n")
 
-    if (inherits (x, "data.frame")) {
-        if ("rank" %in% names (x)) {
-            xout <- x$package [seq_len (n)]
-        } else {
-            xout <- list (
-                "text" = x$package [x$text_rank] [seq_len (n)],
-                "code" = x$package [x$code_rank] [seq_len (n)]
-            )
-        }
-        print (xout)
-    } else if (inherits (x, "list")) {
-        xout <- lapply (x, function (i) utils::head (i$package, n))
-        print (xout)
+    if ("rank" %in% names (x)) {
+        xout <- x$package [seq_len (n)]
     } else {
-        print (x)
+        xout <- list (
+            "text" = x$package [x$text_rank] [seq_len (n)],
+            "code" = x$package [x$code_rank] [seq_len (n)]
+        )
     }
+    print (xout)
 }
 
 #' Head method for 'pkgmatch' objects
